6c0716e4bf84c6ed57bcb5f9248080d6ff30d1e1
Maxim Rodionov <rodionov.m.dev@gmail.com>
2020-04-05 18:46:13 +0300

    MealAjaxController impl

---
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealAjaxController.java
@@ -0,0 +1,49 @@
+package ru.javawebinar.topjava.web.meal;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.to.MealTo;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("/ajax/meals")
+public class MealAjaxController extends AbstractMealController{
+
+    @Override
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<MealTo> getAll() {
+        return super.getAll();
+    }
+
+    @Override
+    @DeleteMapping("/{id}")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable int id) {
+        super.delete(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void createOrUpdate(@RequestParam(required = false) Integer id,
+                               @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dateTime,
+                               @RequestParam String description,
+                               @RequestParam int calories) {
+        Meal meal = new Meal(id, dateTime, description, calories);
+
+        if (meal.isNew()) {
+            super.create(meal);
+        }
+    }
+
+    @Override
+    public List<MealTo> getBetween(LocalDate startDate, LocalTime startTime, LocalDate endDate, LocalTime endTime) {
+        return super.getBetween(startDate, startTime, endDate, endTime);
+    }
+}
